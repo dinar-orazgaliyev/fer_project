b4f721800ebb2632aa26a984d4692377a15f53f9
Dinar <dior00002@WINDORAZGALIYEV-97424.localdomain>
2025-07-14 21:42:46 +0200

    upds

--- models/convnetfer_model.py
+++ models/convnetfer_model.py
@@ -5,7 +5,7 @@ from typing import List, Callable
 from .base_model import BaseModel
 
 class ConvNetFer(BaseModel):
-    
+
     def __init__(self,input_shape:List[int], num_classes:int, hidden_layers:List[int], activation,dropout,norm_layer):
         super().__init__()
         self.input_channels = input_shape
@@ -14,3 +14,25 @@ class ConvNetFer(BaseModel):
         self.activation = activation
         self.dropout = dropout
         self.norm_layer = norm_layer
+        self.kernel_size = (3,3)
+        self.maxpool_size = (2,2)
+        self.stride_size = (2,2)
+        self.__build_model()
+    
+    def __build_model(self):
+        layers = []
+        for id,hidden_size in enumerate(self.hidden_layers):
+            if id == 0:
+                layers.append(nn.Conv2d(in_channels = 3, out_channels = hidden_size, kernel_size=self.kernel_size,padding=1))
+            else:
+                layers.append(nn.Conv2d(in_channels = self.hidden_layers[id-1], out_channels = hidden_size, kernel_size = self.kernel_size,padding=1))
+            if self.norm_layer:
+                layers.append(self.norm_layer(num_features = hidden_size))
+            if id < len(self.hidden_layers) - 1:
+                layers.append(nn.MaxPool2d(kernel_size = self.maxpool_size, stride = self.stride_size))
+            
+            layers.append(self.activation())
+        fc_input_size = self.hidden_layers[-1] 
+        #layers.append(nn.Linear(fc_input_size,self.num_classes))
+        self.classifier = nn.Linear(fc_input_size, self.num_classes)
+        self.layers = nn.Sequential(*layers) 
